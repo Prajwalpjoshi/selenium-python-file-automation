e8b242ced7d77f2ecce537c277d184026547a746
Prajwal Joshi <prajwaljoshi1856@gmail.com>
2026-03-16 16:31:15 +0530

    Fix Chrome headless setup for GitHub Actions

--- utils/driver_setup.py
+++ utils/driver_setup.py
@@ -7,11 +7,17 @@ def get_driver():
 
     options = webdriver.ChromeOptions()
 
+    # Required for GitHub Actions Linux environment
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+
     download_path = os.path.abspath("downloads")
 
     prefs = {
         "download.default_directory": download_path,
-        "download.prompt_for_download": False
+        "download.prompt_for_download": False,
     }
 
     options.add_experimental_option("prefs", prefs)
